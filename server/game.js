b118b6eecef002b2ed4f7e2ea7394283e25bac8a
Yi Zhao <zhao1max@gmail.com>
2016-05-08 09:17:59 -0400

    add new word generator

--- server/game.js
+++ server/game.js
@@ -2,13 +2,24 @@
 
 let Game = function () {
   this.answer = '';
+  this._wordList = ['apple', 'elephant', 'cup', 'computer', 'house'];
+  this._wordsGen = this._nextWord(this._wordList);
 };
 
 Game.prototype.match = function(guess) {
   return this.answer.toLowerCase() === guess.toLowerCase();
 };
 Game.prototype.newWord = function () {
-  this.answer = 'answer';
+  this.answer = this._wordsGen.next().value;
+};
+
+Game.prototype._nextWord = function* (list) {
+  let index = 0;
+  while (index < list.length) {
+    yield list[index];
+    index += 1;
+    if (index === list.length) index = 0
+  }
 };
 
 module.exports = Game;
